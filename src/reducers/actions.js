654e49d2932f995df5f879f34b6993df72b0d98f
cladams0203 <cladams0203@gmail.com>
2020-05-26 13:02:51 -0400

    added redux and reducers

--- src/reducers/actions.js
+++ src/reducers/actions.js
@@ -1,12 +1,3 @@
-import axios from "axois";
-
-export function api() {
-  const token = localStorage.getItem("token");
-  return axios.create({
-    baseUrl: "base",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `${token}`,
-    },
-  });
-}
+export const LOGIN_START = "LOGIN_START";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAIL = "LOGIN_FAIL";
