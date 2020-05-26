1df1217d4184c9c8d3cf7253cba54183af52ac1a
karina-rodriguez <karminer60@gmail.com>
2020-05-26 11:03:03 -0700

    added some template code on App.js module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,46 @@
-import React from "react";
+
 
 import "./App.css";
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+
+const initialFormValues = {
+ 
+  username: '',
+  email: '',
+
+  password: '',
+  
+  role: '',
+ 
+  civil: '',
+  
+  termsOfUse: false,
+    
+  
+}
+
+const initialFormErrors = {
+  username: '',
+  email: '',
+  password: '',
+  role: '',
+  civil: '',
+}
+
+const initialUsers = []
+const initialDisabled = true
 
 function App() {
+  const [users, setUsers] = useState(initialUsers);
+  const [formValues, setFormValues] = useState(initialFormValues);
+ 
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(initialDisabled);
+
   return <div className="App"></div>;
 }
 
