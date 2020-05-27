fb0d793637c1f80b39bb83262c3fa04bffa984ad
karina-rodriguez <karminer60@gmail.com>
2020-05-27 13:06:17 -0700

    small edits to all modules

--- src/User.js
+++ src/User.js
@@ -6,7 +6,7 @@ function User({ details }) {
     }
   
     return (
-      <div className='friend container'>
+      <div className='user container'>
         <h2>{details.username}</h2>
         <p>Email: {details.email}</p>
         
