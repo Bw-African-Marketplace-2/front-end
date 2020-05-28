665e64ecd54463e0780656d0d4b253f69f386d33
karina-rodriguez <karminer60@gmail.com>
2020-05-28 09:30:17 -0700

    created Product.js file

---
+++ src/Product.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Prodcut({ details }) {
+    if (!details) {
+      return <h3>Working fetching your user&apos;s details...</h3>
+    }
+  
+    return (
+      <div className='product container'>
+        <h2>Product</h2>
+        
+       
+      </div>
+    )
+  }
+  
+  export default User;
