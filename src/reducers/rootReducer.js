40448a18d4f01bd22499b62acc8c9b8ce8e1b607
cladams0203 <cladams0203@gmail.com>
2020-05-28 13:05:23 -0400

    storing user object in global state

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
-import { userReducer as user } from "./userReducer";
+import { userReducer } from "./userReducer";
 
 export default combineReducers({
-  user,
+  userReducer,
 });
