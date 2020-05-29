43226c5cd2229501d5db6d7f50eff035b2395ece
karina-rodriguez <karminer60@gmail.com>
2020-05-29 09:04:27 -0700

    styled AddProduct.js and ProductCard.js

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -2,6 +2,47 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { api } from "./auth/api";
+import styled from 'styled-components';
+
+const StyledAddProduct = styled.div`
+  color: black;
+  text-decoration: none;
+  .productContainer {
+    width: 500px;
+    font-family: "Barlow", sans-serif;
+  }
+  img {
+    width: 500px;
+  }
+  .header{
+    font-family: 'Barlow', sans-serif;
+    background-color: #003049;
+  }     
+    
+  
+  .header nav{
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: no-wrap;
+    flex-direction: row;
+    padding: 2% 12%;
+    border-bottom: 0.2rem solid #003049;
+    color: white;
+       
+  }
+  
+  .header nav a{
+  font-size: 1.6 rem;
+  text-decoration: none;
+  color: #edf2f4;
+  padding: 20px;
+  
+  }
+  h1{
+    font-family: 'Barlow', sans-serif;
+  }
+`;
 
 export default function AddProduct() {
   const { id } = useSelector((state) => state.userReducer.user);
@@ -33,6 +74,20 @@ export default function AddProduct() {
   };
 
   return (
+    <StyledAddProduct>
+      <div className='header'>
+         <nav>
+          <a href="https://africanmarketplace-2.netlify.app/index.html">Home</a>
+          <a href="https://africanmarketplace-2.netlify.app/about.html" target="_blank">About Us</a>
+         </nav>
+         
+        </div> 
+        <br/>
+        <br/>
+      <h1>Add New Product</h1>
+      <br/>
+      <br/>
+      <br/>
     <form onSubmit={handleSubmit}>
       <input
         name="product_name"
@@ -40,25 +95,35 @@ export default function AddProduct() {
         onChange={handleChange}
         placeholder="Product Name"
       />
+      <br/>
+      <br/>
+    
       <input
         name="image"
         value={form.image}
         onChange={handleChange}
         placeholder="Image URL"
       />
+      <br/>
+      <br/>
       <input
         name="description"
         value={form.description}
         onChange={handleChange}
         placeholder="Product Description"
       />
+      <br/>
+      <br/>
       <input
         name="price"
         value={form.price}
         onChange={handleChange}
         placeholder="Product Price"
       />
+      <br/>
+      <br/>
       <button type="submit">Add New Product</button>
     </form>
+    </StyledAddProduct>
   );
 }
